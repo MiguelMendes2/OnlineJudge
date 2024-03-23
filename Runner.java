1c53ddef832a67695d77671c4fc8204e193162a7
Miguel Mendes <fc59885@alunos.fc.ul.pt>
2024-03-23 11:49:36 +0000

    first commit

---
+++ Runner.java
@@ -0,0 +1,10 @@
+
+package clubeProgramacao;
+
+import clubeProgramacao.p100.Problem100;
+
+public class Runner {
+    public static void main(String[] args) {
+        Problem100.Runner();
+    }
+}
